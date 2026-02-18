167af2425b84a93587f41fd21761bb1c25d4e9e5
Sai Dhanush <sai.dhanush142@gmail.com>
2026-02-18 19:30:58 +0530

    07:Ai-providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 
 import React from 'react'
 import { requireAuth } from '@/lib/auth-utils'
-import { caller } from '@/trpc/server'
-
+import { caller, trpc } from '@/trpc/server'
+import { useMutation } from '@tanstack/react-query'
+import { Button } from '@/components/ui/button'
 const page = async () => {
   await requireAuth()
   const data = await caller.getUser()
+
+  const testAi = useMutation(trpc.testAi.mutationOptions())
+
+  
   return (
-    <div className='text-red-600  font-bold flex items-center justify-center h-screen'>
-      {JSON.stringify(data)}
+    <div className='text-red-600 font-bold flex flex-col items-center justify-center h-screen gap-4'>
+      <Button onClick={() => testAi.mutate()} disabled={testAi.isPending}>Test AI</Button>
     </div>
   )
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,3 +1,4 @@
+import { inngest } from '@/inngest/client';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import {prisma} from '@/lib/db';
 export const appRouter = createTRPCRouter({
@@ -7,6 +8,12 @@ export const appRouter = createTRPCRouter({
         id: ctx.session.user.id
       }
     })
+  }),
+  testAi: protectedProcedure.mutation(async () =>{
+    await inngest.send({
+      name: 'execute/ai',
+    });
+    return {success: true, message: 'Job is going'}
   })
 });
 export type AppRouter = typeof appRouter;
